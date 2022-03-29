205932c688ea2d8f1a35bad764ed6ed9121d66ff
Ahad hossain <ahadxx99@gmail.com>
2022-03-30 00:05:54 +0800

    aennrr

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
+import Banner from "./Main/Banner/Banner"
 
 
 const IndexPage = () => (
   <Layout>
+    <Banner></Banner>
    
   </Layout>
 )
