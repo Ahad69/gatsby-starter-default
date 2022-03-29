4f7163deb45c3b5464a54813086bfafbf327a70b
Ahad hossain <ahadxx99@gmail.com>
2022-03-30 02:36:43 +0800

    almost

--- src/pages/Main/Banner/Banner.js
+++ src/pages/Main/Banner/Banner.js
@@ -3,25 +3,25 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div class="banner">
-          <div class="number text-center text-white">
-            <h4 class="fw-bold">Start Pest Control for $99*</h4>
-            <h1 class="fw-bold">
-              <a class="call" href="tel:(888) 888-8888">(888) 888-8888</a>
+        <div className="banner">
+          <div className="number text-center text-white">
+            <h4 className="fw-bold">Start Pest Control for $99*</h4>
+            <h1 className="fw-bold">
+              <a className="call" href="tel:(888) 888-8888">(888) 888-8888</a>
             </h1>
           </div>
-          <div class="poster pt-5">
-            <div class="container">
-              <div class="homeHeading text-center">
-                <h1 class="heading text-white">
+          <div className="poster pt-5">
+            <div className="container">
+              <div className="homeHeading text-center">
+                <h1 className="heading text-white">
                   <span>Eliminate</span> Pests Today
                 </h1>
-                <h1 class="fw-bold text-white">
+                <h1 className="fw-bold text-white">
                   Fast & Relentless Protection You Can Rely On
                 </h1>
                 <br />
                 <div id="form" >
-                  <input class="input" type="text" id="input" placeholder="Email" />
+                  <input className="input" type="text" id="input" placeholder="Email" />
                   <button id="submit-button"   type="submit">
                     Get My Free Quote
                   </button>
--- src/pages/Main/Guarantee/Guarantee.js
+++ src/pages/Main/Guarantee/Guarantee.js
@@ -4,28 +4,28 @@ import '../Banner/Banner.css'
 const Guarantee = () => {
     return (
         <div>
-             <hr class="p-1 hr" />
-      <div class="guarantee container">
+             <hr className="p-1 hr" />
+      <div className="guarantee container">
         <img src="https://i.ibb.co/LJYTV3P/png.png" alt=""/>
-        <h3 class="ms-4">Guarantee means you can relax knowing we'll nix your pest problem, and keep it nixed. And if pests come back between treatments, so will we - at no additional cost to you.</h3>
+        <h3 className="ms-4">Guarantee means you can relax knowing we'll nix your pest problem, and keep it nixed. And if pests come back between treatments, so will we - at no additional cost to you.</h3>
       </div>
-      <hr class="p-1 hr" />
+      <hr className="p-1 hr" />
 
       <section>
-        <h1 class="fw-bold text-center pt-5">Trust Your Local</h1>
-        <div class="local-trust pb-3 container">
+        <h1 className="fw-bold text-center pt-5">Trust Your Local</h1>
+        <div className="local-trust pb-3 container">
           <img src="https://terminix.housemethod.com/wp-content/uploads/2020/07/team-years-rebrand.jpg" alt=""/>
-          <div class="container ms-4">
-            <p class="local-trust-para">With 10,000+ expertly trained team members nationwide and over 90 years in business, nobody knows pests and termites like Terminix.</p>
+          <div className="container ms-4">
+            <p className="local-trust-para">With 10,000+ expertly trained team members nationwide and over 90 years in business, nobody knows pests and termites like Terminix.</p>
             <br />
-            <p class="targeted">Terminix® is an industry leader committed to protecting your home against termites and pests with its innovative new technologies. Their team will do whatever it takes to keep pests out, so you can rest easy knowing they’ve got your home covered. For more information, call us at (866) 935-6258.</p>
+            <p className="targeted">Terminix® is an industry leader committed to protecting your home against termites and pests with its innovative new technologies. Their team will do whatever it takes to keep pests out, so you can rest easy knowing they’ve got your home covered. For more information, call us at (866) 935-6258.</p>
           </div>
         </div>
       </section>
       <section id="bottom-form">
-        <div class="bottom-form text-center">
+        <div className="bottom-form text-center">
           <form>
-            <input class="input" type="text" placeholder="Email" />
+            <input className="input" type="text" placeholder="Email" />
             <button id="submit-button" type="submit">
               Get My Free Quote
             </button>
