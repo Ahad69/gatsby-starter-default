f9b95d7134756ee16be84dc0aea291513602a25a
Ahad hossain <ahadxx99@gmail.com>
2022-03-30 02:11:37 +0800

    carosell

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/layout"
 import Banner from "./Main/Banner/Banner"
 import Slider from "./Main/Slider/Slider"
