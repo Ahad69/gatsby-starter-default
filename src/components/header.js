36a73140d5cb2ca04b221032f8411264c1274d45
Ahad hossain <ahadxx99@gmail.com>
2022-03-30 01:04:58 +0800

    banner

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,11 @@ const Header = ({ siteTitle }) => (
     }}
   >
     <div
-    
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
