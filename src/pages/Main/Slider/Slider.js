6f3679799f0a9ea2cd7c252d18dd54af7d7f6fd2
Ahad hossain <ahadxx99@gmail.com>
2022-03-30 01:36:19 +0800

    slider

--- src/pages/Main/Slider/Slider.js
+++ src/pages/Main/Slider/Slider.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import '../Banner/Banner.css'
 
 const Slider = () => {
     return (
         <div>
-            
+             <section class="container">
+        <div class="row mt-3 container g-4">
+          <div class="col">
+            <div class="card border-0">
+              <img
+                src="https://terminix.housemethod.com/wp-content/themes/terminix/images/partners/bbb.jpg"
+                class="card-img-top img-fluid"
+                alt="..."
+              />
+            </div>
+          </div>
+          <div class="col">
+            <div class="card border-0">
+              <img
+                src="https://terminix.housemethod.com/wp-content/themes/terminix/images/rebrand/partners/90-plus-years-experience.jpg"
+                class="card-img-top img-fluid"
+                alt="..."
+              />
+            </div>
+          </div>
+          <div class="col">
+            <div class="card border-0">
+              <img
+                src="https://terminix.housemethod.com/wp-content/themes/terminix/images/rebrand/partners/24-7-support.jpg"
+                class="card-img-top img-fluid"
+                alt="..."
+              />
+            </div>
+          </div>
+          <div class="col">
+            <div class="card border-0">
+              <img
+                src="https://terminix.housemethod.com/wp-content/themes/terminix/images/rebrand/partners/expertly-trained-technicians.jpg"
+                class="card-img-top img-fluid"
+                alt="..."
+              />
+            </div>
+          </div>
+        </div>
+      </section>
         </div>
     );
 };
