73123b4c2d16c801543a8e168067d63cf614a939
Ahad hossain <ahadxx99@gmail.com>
2022-03-30 02:01:54 +0800

    carosell

---
+++ src/pages/Main/Carosel/Carosel.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import '../Banner/Banner.css'
+
+const Carosel = () => {
+    return (
+        <section class="mt-5 pb-3">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner ">
+            <div class="carousel-item  active">
+              <div class="row container m-auto">
+                <div class="col container  review-item">
+                    <div class="star">
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt="" />
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt="" />
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt="" />
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt="" />
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt="" />
+                    </div>
+                    <div class="content">
+                      <h1 class="review">Excellent Customer Service</h1>
+                      <p>"Great customer service! Just started using Terminix due to our plague of ants this year. Great rate to get started and excellent customer service from sales to technician! I’ve already passed the word on to my neighbors." <br />
+                        —  V.H., from Yelp
+                      </p>
+                    </div>
+               
+                </div>
+                <div class="col review-item">
+                    <div class="star">
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt="" />
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt="" />
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt="" />
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                    </div>
+                    <div class="content">
+                      <h1 class="review">Kept All The Bugs Away</h1>
+                      <p>"If any [termites] show up, they take care of them and I don’t see the termites again. The techs who arrive have always been professional. They have kept all the bugs away so far."<br />
+                        —  V.H., from Yelp</p>
+                    </div>
+                </div>
+            </div>
+
+         </div>
+            <div class="carousel-item">
+              <div class="row container m-auto">
+                <div class="col container  review-item">
+                    <div class="star">
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                    </div>
+                    <div class="content">
+                      <h1 class="review">Very Kind And Helpful</h1>
+                      <p>"Great customer service! Just started using Terminix due to our plague of ants this year. Great rate to get started and excellent customer service from sales to technician! I’ve already passed the word on to my neighbors."<br />
+                        —  V.H., from Yelp</p>
+                    </div>
+               
+                </div>
+                <div class="col review-item">
+                    <div class="star">
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                      <img width="20px" src="https://i.ibb.co/f0HFTG0/yellow-star-no-background-free-star-icon-symbol-cross-star-symbol-transparent-png-2610204-1.png" alt=""/>
+                    </div>
+                    <div class="content">
+                      <h1 class="review">Very polite and wearing a mask</h1>
+                      <p>"I told him I work in my garage with the door open and he made sure to get that side of the house thoroughly.” <br />
+                        —  V.H., from Yelp</p>
+                    </div>
+                </div>
+            </div>
+        
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon  me-5" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon  ms-5" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+        </div>
+      </section>
+    );
+};
+
+export default Carosel;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Banner from "./Main/Banner/Banner"
 import Slider from "./Main/Slider/Slider"
+import Carosel from "./Main/Carosel/Carosel"
 
 
 const IndexPage = () => (
   <Layout>
     <Banner></Banner>
     <Slider></Slider>
+    <Carosel></Carosel>
   </Layout>
 )
 
