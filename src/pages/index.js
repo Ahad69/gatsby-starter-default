0481619dd503fd4ca3d4c6eef8eacaa9b4a2f802
Ahad hossain <ahadxx99@gmail.com>
2022-03-30 01:39:08 +0800

    slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,13 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Banner from "./Main/Banner/Banner"
+import Slider from "./Main/Slider/Slider"
 
 
 const IndexPage = () => (
   <Layout>
     <Banner></Banner>
-   
+    <Slider></Slider>
   </Layout>
 )
 
